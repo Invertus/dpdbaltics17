0d25d812ce957ee89618fbf52c338b088dfe0214
tomjas1997 <tomas.jasulaitis@invertus.eu>
2023-06-15 14:42:23 +0300

    DGS-238 shipping tab improvements

--- src/Util/FileDownload.php
+++ src/Util/FileDownload.php
@@ -44,41 +44,37 @@ class FileDownload
         $file = sprintf('%s.%s', $name, $extension);
         $file = htmlentities($file, ENT_QUOTES | ENT_HTML401, 'UTF-8');
 
-        $tmpFolder = $this->module->getLocalPath().'tmp/';
+        $tmpFolder = $this->module->getLocalPath() . 'tmp/';
 
-        file_put_contents($tmpFolder.$file, $content);
+        $filePath = $tmpFolder . $file;
 
-        if (ob_get_level() && ob_get_length() > 0) {
-            ob_clean();
+        file_put_contents($filePath, $content);
+
+        if (!file_exists($filePath)) {
+            return;
         }
 
-        if (file_exists($tmpFolder.$file)) {
-            if(ob_get_length() > 0) {
-                ob_clean();
-            }
 
-            header('Content-Description: File Transfer');
-            header('Cache-Control: private, must-revalidate, post-check=0, pre-check=0, max-age=1');
-            header('Pragma: public');
-            header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
-            header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
-            header('Content-Type: application/'.$extension);
-            header('Content-Transfer-Encoding: Binary');
-            header('Content-Length:'.filesize($tmpFolder.$file));
-            header('Accept-Ranges: bytes');
-
-            switch (Configuration::get(Config::LABEL_PRINT_OPTION)) {
-                case 'browser_print':
-                    header('Content-Disposition: inline; filename='.$file);
-                    break;
-                case 'download':
-                default:
-                    header('Content-Disposition: attachment; filename='.$file);
-                    break;
-            }
-
-            readfile($tmpFolder.$file);
-            unlink($tmpFolder.$file);
+        if (\Configuration::get(Config::LABEL_PRINT_OPTION) === Config::PRINT_OPTION_BROWSER) {
+            header('Content-type:application/' . $extension);
+        } else {
+            header('Content-Type: application/octet-stream');
         }
+
+        header('Content-Description: File Transfer');
+        header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: private, must-revalidate, post-check=0, pre-check=0, max-age=1');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($filePath));
+        header('Content-disposition: inline; filename="' . basename($file) . '"');
+        header('content-Transfer-Encoding:binary');
+        header('Accept-Ranges:bytes');
+        header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+
+        readfile($filePath);
+        unlink($filePath);
+
+        exit;
     }
 }
